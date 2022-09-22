d4a8173a1df9303afd410476c883aac41451c32a
michalskimateusz <michalskimateusz@outlook.com>
2022-09-22 17:40:17 +0200

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>ForX</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>ForX</h1>
+
+    </div>
+  )
+}
+
+export default Home
